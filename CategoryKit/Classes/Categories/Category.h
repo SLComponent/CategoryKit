40a4866d48ad82dcb9462e43c0ca49a85fe799ae
fengsonglin <15013339531@163.com>
2018-04-15 19:07:38 +0800

    Category.h

--- CategoryKit/Classes/Categories/Category.h
+++ CategoryKit/Classes/Categories/Category.h
@@ -8,5 +8,28 @@
 #ifndef Category_h
 #define Category_h
 
+//导入所有需要用到的头文件
+#import "LTConstant.h"
+#import "Macro.h"
+#import "NSError+LTWeb.h"
+#import "NSObject+SLCache.h"
+#import "NetWorkMacro.h"
+#import "NSObject+SXRuntime.h"
+#import "NSString+Extension.h"
+#import "SLCircleLoadView.h"
+#import "SLErrorLoadView.h"
+#import "UIALertAction+Extension.h"
+#import "UIButton+Extension.h"
+#import "UICollectionViewCell.h"
+#import "UIColor+Extension.h"
+#import "UIFont+Extension.h"
+#import "UIImage+Extension.h"
+#import "UIImageView+Extension.h"
+#import "UILabel+Extension.h"
+#import "UINavigation+SLFixSpace.h"
+#import "UITableViewCell+Extension.h"
+#import "UITextField+Extension.h"
+#import  "UIView+Extension.h"
+
 
 #endif /* Category_h */
